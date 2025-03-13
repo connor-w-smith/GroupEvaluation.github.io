ccbf6993b1ea4c957beb6fb3c3fe3732426c2683
connor-w-smith <connorwadesmith@gmail.com>
2025-03-13 16:55:52 -0500

    Added js file
    I am wanting to implement vanilla javascript to be able to switch through forms. I have a couple ideas that I am trying to be able to acomplish this.

---
+++ app.js
@@ -0,0 +1,9 @@
+/*
+    Author: Connor Smith & Damien Owen
+    Date: 3/13/2025
+    Description: Javascript File for Group Evaluation Website.
+*/
+
+// When the user click's Student input, the form will unhide
+// let selectStudent = document.getElementById("selectForm")
+// let studentForm1 = document.getElementById("studentForm")
